98fb8d06e973573ea1f28d1722269e95f4d1bacb
Leonardo Dias <leoddias@live.com>
2020-01-30 17:53:30 -0300

    Add logger in app functions

--- api.py
+++ api.py
@@ -1,11 +1,18 @@
-from flask_api import FlaskAPI, exceptions, status
-import twitter, json, os
+from flask_api import FlaskAPI, status
+import twitter, json, os, graypy, logging, time
 
 #Global Vars
 app = FlaskAPI(__name__)
 given_tags = ["openbanking", "apifirst", "devops", "cloudfirst", "microservices", "apigateway", "oauth", "swagger", "raml", "openapis"]
 G_MAX_SEARCH = "100"
 G_MAX_USERS = 5
+
+#Loggin Setup (GrayLog)
+logger = logging.getLogger('logger')
+logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s')
+logger.setLevel(logging.INFO)
+handler = graypy.GELFTCPHandler('graylog', 12201)
+logger.addHandler(handler)
 
 #Twitter Setup
 api = twitter.Api(consumer_key=os.getenv('consumer_key'),
@@ -19,14 +26,16 @@ api = twitter.Api(consumer_key=os.getenv('consumer_key'),
 def GetByTag(search_tag):
 
     if search_tag is None:
-        print("Error Empty Tag!")
-        raise exceptions.NotFound()
+        logger.error("Empty", extra={'function': 'GetByTag', 'latency': 0})
         return '', status.HTTP_204_NO_CONTENT
 
     try:
         result_json = api.GetSearch(raw_query="q="+search_tag+"&result_type=recent&count="+G_MAX_SEARCH, return_json=True)
     except:
+        logger.error("Unknown", extra={'function': 'GetByTag', 'latency': 0})
         return '', status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    logger.info(search_tag, extra={'function': 'GetByTag', 'latency': 0})
 
     return result_json, status.HTTP_200_OK
 
@@ -52,10 +61,13 @@ def HighFollower():
                     "tag": tag
                 })
     except:
+        logger.error("Unknown", extra={'function': 'HighFollower', 'latency': 0})
         return '', status.HTTP_500_INTERNAL_SERVER_ERROR            
 
     #Top 5 followed users
     top_users_json = sorted(data, key = lambda usr: usr['followers'], reverse=True)[:G_MAX_USERS] 
+
+    logger.info(top_users_json, extra={'function': 'HighFollower', 'latency': 0})
 
     return { 'result': top_users_json }, status.HTTP_200_OK           
     
